82596d3bbf6ab27e1c06f98f16cb9e65c8c30671
ToHold <38291374+ToHold@users.noreply.github.com>
2024-09-29 01:49:31 +0200

    Update PluginInfo.cs

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,6 +4,6 @@
     {
         public const string PLUGIN_GUID = "WankulCrazyPlugin";
         public const string PLUGIN_NAME = "WankulCrazy";
-        public const string PLUGIN_VERSION = "0.0.1";
+        public const string PLUGIN_VERSION = "1.0.0";
     }
 }
