962368783a89578b3357d752d6f3591f483da3d9
Hurtem <62056819+Hurtem@users.noreply.github.com>
2025-02-26 14:59:47 +0100

    Ajout des 6 affiches

--- patch/WindowsPosters.cs
+++ patch/WindowsPosters.cs
@@ -9,33 +9,83 @@ namespace WankulCrazyPlugin.patch
 
     public class WindowsPosters
     {
+        //public static void Init()
+        //{
+        //    Transform Windows_Transform = Plugin.GetByPathIn("Level_Environment_Grp", "StoreModel_Group/Windows door");
+
+        //    GameObject poster = new GameObject("Poster");
+
+        //    SpriteRenderer spriteRenderer = poster.AddComponent<SpriteRenderer>();
+
+        //    string texturePath = Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Commun.png");
+        //    Texture2D texture = LoadPNG(texturePath);
+
+        //    if (texture != null)
+        //    {
+        //        spriteRenderer.sprite = TextureToSprite(texture);
+
+        //        Plugin.Logger.LogInfo("Sprite créé avec succès");
+        //    }
+        //    else
+        //    {
+        //        Plugin.Logger.LogInfo("Échec du chargement de la texture");
+        //    }
+
+        //    poster.transform.SetParent(Windows_Transform, false);
+
+        //    poster.transform.localPosition = new Vector3(-0.4545f, 0.7963f, 0.5f);
+        //    poster.transform.localScale = new Vector2(0.05f, 0.05f);
+        //    poster.transform.localRotation = Quaternion.Euler(0, 180, 0);
+        //}
         public static void Init()
         {
             Transform Windows_Transform = Plugin.GetByPathIn("Level_Environment_Grp", "StoreModel_Group/Windows door");
 
-            GameObject poster = new GameObject("Poster");
-
-            SpriteRenderer spriteRenderer = poster.AddComponent<SpriteRenderer>();
-
-            string texturePath = Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "CardBox.png");
-            Texture2D texture = LoadPNG(texturePath);
-
-            if (texture != null)
+            Vector3[] positions = new Vector3[]
             {
-                spriteRenderer.sprite = TextureToSprite(texture);
+                new Vector3(-0.4545f, 0.7963f, 0.5f),
+                new Vector3(1.7419f, 0.7963f, 0.5f),
+                new Vector3(3.8383f, 0.7963f, 0.5f),
+                new Vector3(5.8347f, 0.7963f, 0.5f),
+                new Vector3(7.86f, 0.7963f, 0.5f),
+                new Vector3(9.85f, 0.7963f, 0.5f)
+            };
 
-                Plugin.Logger.LogInfo("Sprite créé avec succès");
-            }
-            else
+            string[] texturePaths = new string[]
             {
-                Plugin.Logger.LogInfo("Échec du chargement de la texture");
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Commun.png"),
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Rare.png"),
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Shiny.png"),
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Legendaire.png"),
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Gold.png"),
+                Path.Combine(Plugin.GetPluginPath(), "data", "sprites", "Affiche_Pack_Gamer.png")
+            };
+
+            for (int i = 0; i < positions.Length; i++)
+            {
+                string posterName = "Poster" + (i + 1);
+                GameObject poster = new GameObject(posterName);
+
+                SpriteRenderer spriteRenderer = poster.AddComponent<SpriteRenderer>();
+
+                string texturePath = texturePaths[i];
+                Texture2D texture = LoadPNG(texturePath);
+
+                if (texture != null)
+                {
+                    spriteRenderer.sprite = TextureToSprite(texture);
+                    Plugin.Logger.LogInfo(posterName + " : Sprite créé avec succès");
+                }
+                else
+                {
+                    Plugin.Logger.LogInfo(posterName + " : Échec du chargement de la texture");
+                }
+
+                poster.transform.SetParent(Windows_Transform, false);
+                poster.transform.localPosition = positions[i];
+                poster.transform.localScale = new Vector2(0.048f, 0.048f);
+                poster.transform.localRotation = Quaternion.Euler(0, 180, 0);
             }
-
-            poster.transform.SetParent(Windows_Transform, false);
-
-            poster.transform.localPosition = new Vector3(0, 1.2618f, 0.5f);
-            poster.transform.localScale = new Vector2(0.2f, 0.2f);
-            poster.transform.localRotation = Quaternion.Euler(0, 180, 0);
         }
 
         public static Texture2D LoadPNG(string filePath)
